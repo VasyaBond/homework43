dc20f938bda94080c43d1f06a31f37b91bad2c00
Jeka <e.g.bondarev@gmail.com>
2023-10-01 11:59:02 +0300

    new com

---
+++ seminar4/homework27/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string? input = Console.ReadLine();
+
+        long number;
+        try
+        {
+            number = Convert.ToInt64(input);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка ввода. Введите целое число.");
+            return;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Ошибка: введено слишком большое или слишком маленькое число.");
+            return;
+        }
+
+        long sum = 0;
+        long tempNumber = Math.Abs(number);
+
+        while (tempNumber > 0)
+        {
+            long digit = tempNumber % 10;
+            sum += digit;
+            tempNumber /= 10;
+        }
+
+        Console.WriteLine("Сумма цифр в числе: " + sum);
+    }
+}
---
+++ seminar4/homework29/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив из 8 элементов
+        int[] numbers = new int[8];
+
+        // Заполняем массив элементами
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write("Введите элемент " + (i + 1) + ": ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int number = Convert.ToInt32(input);
+                numbers[i] = number;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка ввода. Введите целое число.");
+                return;
+            }
+        }
+
+        // Выводим массив на экран
+        Console.Write("Массив: [");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i]);
+            if (i < numbers.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
